58cd67c561c44c56714ced85d6714ccaf9bdeaeb
AruberutoEXE <albertoblsr@gmail.com>
2023-04-04 18:31:11 +0200

    productoDao
    creacion de producto dao para operaciones de lectura

--- Cexupo/src/java/DAO/ProductoDao.java
+++ Cexupo/src/java/DAO/ProductoDao.java
@@ -5,10 +5,30 @@
  */
 package DAO;
 
+import Hibernate.HibernateUtil;
+import Hibernate.Producto;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 /**
  *
  * @author alber
  */
 public class ProductoDao {
-    
+    public Producto getProducto(int id) {
+        Session sesion = HibernateUtil.getSessionFactory().getCurrentSession();
+        org.hibernate.Transaction tx = sesion.beginTransaction();
+        Query q = sesion.createQuery("from Producto where id='"+id+"'");
+        Producto u = (Producto) q.uniqueResult();
+        tx.commit();
+        return u;
+    }
+    public Producto getAllProductos() {
+        Session sesion = HibernateUtil.getSessionFactory().getCurrentSession();
+        org.hibernate.Transaction tx = sesion.beginTransaction();
+        Query q = sesion.createQuery("from Producto");
+        Producto u = (Producto) q.list();
+        tx.commit();
+        return u;
+    }
 }
