a3c2081d90c2a93e609412cff1f5d83daa9dfe58
AruberutoEXE <albertoblsr@gmail.com>
2023-06-01 13:15:42 +0200

    crear chat action

---
+++ Cexupo/src/java/Actions/CreateChatAction.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Actions;
+
+import DAO.UsuarioDao;
+import Hibernate.ChatId;
+import Hibernate.Mensaje;
+import com.opensymphony.xwork2.ActionSupport;
+import java.util.List;
+import java.util.Map;
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.SessionAware;
+
+/**
+ *
+ * @author alber
+ */
+public class CreateChatAction extends ActionSupport implements SessionAware{
+    
+    private List<Mensaje> mensajes;
+    private SessionMap<String, Object> sessionMap;
+    public CreateChatAction() {
+    }
+    
+    public String execute() throws Exception {
+        String username=(String)sessionMap.get("username");
+         UsuarioDao uDao = new UsuarioDao();
+            mensajes = uDao.getMensajesChat(chatId);
+        return SUCCESS;
+        
+    }
+
+    @Override
+    public void setSession(Map<String, Object> map) {
+        sessionMap = (SessionMap) map;
+    }
+    
+}
--- Cexupo/src/java/DAO/ChatDao.java
+++ Cexupo/src/java/DAO/ChatDao.java
@@ -36,4 +36,24 @@ public class ChatDao {
         sesion.close();
         return salida;
     }
+     public static boolean getChat(Long idProducto,String idUsuario){
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        boolean salida = true;
+        org.hibernate.Transaction tx = null;
+         ChatId chat =new ChatId();
+        chat.setIdProducto(idProducto);
+        chat.setIdUsuario(idUsuario);
+        try{
+            tx = sesion.beginTransaction();
+            sesion.save(chat);
+            tx.commit();
+        }catch(Exception ex){
+            if(tx != null){
+                tx.rollback();
+            }
+            salida = false;
+        }
+        sesion.close();
+        return salida;
+    }
 }
